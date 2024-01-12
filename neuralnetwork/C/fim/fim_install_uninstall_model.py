64e36321a1547be88319218df8050bf532ba2436
yonatank <yonatank@byu.edu>
2024-01-12 12:54:45 -0700

    Add a script to install/uninstall model ensembles for FIM method

---
+++ neuralnetwork/C/fim/fim_install_uninstall_model.py
@@ -0,0 +1,57 @@
+"""This script ins exclusively used to install or uninstall the model ensembles for this
+method. The model ensembles and the KIM models need to be written prior to running this
+script.
+"""
+
+##########################################################################################
+from pathlib import Path
+import json
+import sys
+import subprocess
+
+
+argv = sys.argv
+if len(argv) > 1:
+    # Mode argument is speciied
+    mode = argv[1]
+else:
+    mode = "install"
+
+
+##########################################################################################
+# Initial Setup
+# -------------
+
+# Read setting file
+WORK_DIR = Path(__file__).absolute().parent
+ROOT_DIR = WORK_DIR.parent
+with open(ROOT_DIR / "settings.json", "r") as f:
+    settings = json.load(f)
+partition = settings["partition"]
+
+# Directories
+PART_DIR = ROOT_DIR / f"{partition}_partition_data"
+RES_DIR = WORK_DIR / "results" / f"{partition}_partition"
+if not RES_DIR.exists():
+    RES_DIR.mkdir()
+
+
+##########################################################################################
+# Write KIM model
+# ---------------
+for set_idx in range(100):
+    SAMPLE_DIR = RES_DIR / f"{set_idx:03d}"
+    modelname = f"DUNN_C_fimbayes_{set_idx:03d}"
+    modelpath = SAMPLE_DIR / modelname
+    if not modelpath.exists():
+        print(f"{modelname} is not found. Please write this KIM model first.")
+        continue
+
+    if mode == "install":
+        # Install
+        subprocess.run(["kim-api-collections-management", "install", "user", modelpath])
+    elif mode == "uninstall":
+        # Uninstall
+        subprocess.run(
+            ["kim-api-collections-management", "remove", "--force", modelname]
+        )
